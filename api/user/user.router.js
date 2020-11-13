7f0b0757577ed71fb46af5642b4b825dc2b561ab
khesyaa28 <60589604+khesyaa28@users.noreply.github.com>
2020-11-13 22:53:44 +0700

    Update user.router.js

--- api/user/user.router.js
+++ api/user/user.router.js
@@ -1,9 +1,13 @@
 const{
     controllerAddUser,
-    controllerGetUsers
+    controllerGetUsers,
+    controllerUpdateUser,
+    controllerDeleteUser
 } = require("./user.controller");
 
 const router = require("express").Router();
 router.post("/add", controllerAddUser);
 router.get("/get", controllerGetUsers);
+router.patch("/update", controllerUpdateUser);
+router.delete("/delete", controllerDeleteUser);
 module.exports = router;
